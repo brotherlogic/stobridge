74f70d1edb238cb8a361098ed34c9d8aed1aba4c
Simon Tucker <brotherlogic@gmail.com>
2021-05-31 01:39:56 -0700

    Deal with missing config

--- stobridgeapi.go
+++ stobridgeapi.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	rcpb "github.com/brotherlogic/recordcollection/proto"
+	pb "github.com/brotherlogic/stobridge/proto"
 )
 
 //ClientUpdate runs an update
@@ -13,7 +16,10 @@ func (s *Server) ClientUpdate(ctx context.Context, in *rcpb.ClientUpdateRequest)
 	version := (in.ProtoReflect().Descriptor().Fields().Len())
 	config, err := s.read(ctx)
 	if err != nil {
-		return nil, err
+		if status.Convert(err).Code() != codes.InvalidArgument {
+			return nil, err
+		}
+		config = &pb.Config{Tracked: make(map[int32]int32)}
 	}
 	s.Log(fmt.Sprintf("Updating %v with %v and then %v", in.GetInstanceId(), version, config.Tracked[in.GetInstanceId()]))
 	return &rcpb.ClientUpdateResponse{}, s.save(ctx, config)
